75aa96a0f0b12cb8aa9873dd86960a2a0217f5cd
luciob <lucio.bordonaro@gmail.com>
2023-06-29 11:21:23 +0200

    feat: implemented Grid static view
    #1

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,41 +1,36 @@
 import React, { FC } from "react";
 import { Group, Line, Text } from "react-konva";
 
-export interface User {
+export interface Category {
   id: number;
   label: string;
   color: string;
 }
 
 interface GridProps {
+  categories: Category[];
+  columnsCount: number;
+  columnWidth: number;
   height: number;
+  hoursResolution: number;
   width: number;
-  resolutionFactor: number;
-  users: User[];
 }
 
-const Grid: FC<GridProps> = ({ height, width, resolutionFactor, users }) => {
-  console.log("=> width", { width });
-
-  const headerRowWidth = 200;
-
-  const timeslots = new Array(resolutionFactor).fill("");
-
-  const slotSize = (width - headerRowWidth) / resolutionFactor;
+const Grid: FC<GridProps> = ({ categories, columnsCount, columnWidth, height, hoursResolution, width }) => {
+  const columns = new Array(columnsCount).fill("").map((v, index) => index * hoursResolution);
 
   return (
     <Group>
-      {[{ id: -1, label: "Header" }, ...users].map((heading, index) => (
+      {[{ id: -1, label: "Header" }, ...categories].map((heading, index) => (
         <Group key={`heading-${heading.id}`}>
           <Line x={0} y={50 * (index + 1)} points={[0, 0, width, 0]} stroke="black" />
-          <Text y={50 * index} text={`Row: ${heading.label}`} />
         </Group>
       ))}
-      <Line x={headerRowWidth} y={0} points={[0, 0, 0, height]} stroke="blue" />
-      {timeslots.map((slot, index) => (
+      <Line points={[0, 0, 0, height]} stroke="blue" />
+      {columns.map((column, index) => (
         <Group key={`timeslot-${index}`}>
-          <Line x={headerRowWidth + slotSize * index} y={0} points={[0, 0, 0, height]} stroke="red" />
-          <Text x={headerRowWidth + slotSize * index} text={`${index}:00`} />
+          <Line x={columnWidth * index} y={0} points={[0, 0, 0, height]} stroke="red" />
+          <Text x={10 + columnWidth * index} y={20} text={`${column}:00`} />
         </Group>
       ))}
     </Group>
--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,57 +1,120 @@
-import React, { CSSProperties, FC, useEffect, useMemo, useRef, useState } from "react";
-import { Layer, Stage } from "react-konva";
+import React, { FC, useEffect, useMemo, useRef, useState } from "react";
+import { Group, Layer, Line, Stage, Text } from "react-konva";
 
-import Grid, { User } from "../Grid";
-import Tasks, { Task } from "../Tasks";
+import Grid, { Category } from "../Grid";
 
-interface TimelineProps {
-  interval: "day";
-  resolution: "hours";
-  tasks: Task[];
-  users: User[];
+interface TimeRange {
+  begin: number;
+  end: number;
 }
 
-const Timeline: FC<TimelineProps> = ({ interval, resolution, tasks, users }) => {
-  const [size, setSize] = useState({ height: 0, width: 0 });
+interface TimelineProps {
+  categories: Category[];
+  columnWidth?: number;
+  hoursResolution?: number;
+  timeRange: TimeRange;
+}
+
+interface StageSize {
+  height: number;
+  width: number;
+}
+
+const CATEGORIES_COLUMN_WIDTH = 200;
+
+const COLUMN_WIDTH = 60;
+
+const DEFAULT_STAGE_SIZE: StageSize = { height: 0, width: 0 };
+
+const Timeline: FC<TimelineProps> = ({
+  categories,
+  columnWidth: externalColumnWidth = COLUMN_WIDTH,
+  hoursResolution = 1,
+  timeRange,
+}) => {
+  const [size, setSize] = useState<StageSize>(DEFAULT_STAGE_SIZE);
   const wrapper = useRef<HTMLDivElement>(null);
+
+  const columnWidth = useMemo(
+    () => (externalColumnWidth < COLUMN_WIDTH ? COLUMN_WIDTH : externalColumnWidth),
+    [externalColumnWidth]
+  );
 
   useEffect(() => {
     if (!wrapper.current) {
       return;
     }
 
-    const { clientHeight, clientWidth } = wrapper.current;
-
-    setSize({ height: clientHeight, width: clientWidth });
+    const { clientHeight: height, clientWidth: width } = wrapper.current;
+    setSize({ height, width });
   }, []);
 
-  const wrapperStyle = useMemo(
-    (): CSSProperties => ({
-      backgroundColor: "aqua",
-      minHeight: "300px",
-      width: "100%",
-    }),
-    []
-  );
+  const timeRangeDurationAsHours = useMemo(() => {
+    const timeRangeDuration = timeRange.end - timeRange.begin;
+    return Math.ceil(timeRangeDuration / (1000 * 60 * 60 * hoursResolution));
+  }, [hoursResolution, timeRange]);
 
-  const resolutionFactor = useMemo(() => {
-    if (interval === "day" && resolution === "hours") {
-      return 24;
-    }
-
-    return 7;
-  }, [interval, resolution]);
+  const stageWidth = useMemo(() => {
+    return columnWidth * timeRangeDurationAsHours;
+  }, [columnWidth, timeRangeDurationAsHours]);
 
   return (
-    <div ref={wrapper} style={wrapperStyle}>
-      <Stage height={size.height} width={size.width} style={{ backgroundColor: "yellow" }}>
-        <Layer>
-          <Grid height={size.height} width={size.width} resolutionFactor={resolutionFactor} users={users} />
-        </Layer>
-        <Layer>
-          <Tasks resolutionFactor={resolutionFactor} tasks={tasks} users={users} width={size.width} />
-        </Layer>
-      </Stage>
+    <div
+      style={{
+        backgroundColor: "black",
+        display: "inline-block",
+        overflow: "scroll",
+        position: "relative",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "white",
+          boxShadow: "4px 4px 32px 1px #0000000f",
+          left: 0,
+          minHeight: "300px",
+          position: "sticky",
+          top: 0,
+          width: "200px",
+          zIndex: 1,
+        }}
+      >
+        <Stage height={size.height} width={CATEGORIES_COLUMN_WIDTH}>
+          <Layer>
+            {[{ id: -1, label: "Header" }, ...categories].map((heading, index) => (
+              <Group x={0} y={0} key={`heading-${heading.id}`}>
+                <Line y={50 * (index + 1)} points={[0, 0, CATEGORIES_COLUMN_WIDTH, 0]} stroke="black" />
+                <Text y={20 + 50 * index} text={heading.label} />
+              </Group>
+            ))}
+          </Layer>
+        </Stage>
+      </div>
+      <div
+        ref={wrapper}
+        style={{
+          backgroundColor: "white",
+          left: "201px",
+          minHeight: "300px",
+          position: "absolute",
+          top: 0,
+          width: stageWidth,
+        }}
+      >
+        <Stage height={size.height} width={stageWidth}>
+          <Layer>
+            <Grid
+              categories={categories}
+              columnsCount={timeRangeDurationAsHours}
+              columnWidth={columnWidth}
+              height={size.height}
+              hoursResolution={hoursResolution}
+              width={stageWidth}
+            />
+          </Layer>
+        </Stage>
+      </div>
     </div>
   );
 };
