2352e30a82a80d57ec52d5dd6633635f65b9e32e
Lucio Bordonaro <61870694+luciob@users.noreply.github.com>
2023-06-29 11:22:30 +0200

    Grid static view (#3)

--- src/components/Timeline/index.stories.tsx
+++ src/components/Timeline/index.stories.tsx
@@ -16,24 +16,15 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
   args: {
-    interval: "day",
-    resolution: "hours",
-    tasks: [
-      { start: "8:00", end: "9:00", label: "Task 1", userId: 1 },
-      { start: "10:00", end: "13:00", label: "Task 2", userId: 1 },
-      { start: "22:00", end: "23:00", label: "Task 3", userId: 1 },
-      { start: "5:00", end: "7:00", label: "Task 4", userId: 2 },
-      { start: "11:00", end: "16:00", label: "Task 5", userId: 2 },
-      { start: "20:00", end: "23:00", label: "Task 6", userId: 2 },
-      { start: "3:00", end: "4:00", label: "Task 7", userId: 3 },
-      { start: "8:00", end: "10:00", label: "Task 8", userId: 3 },
-      { start: "17:00", end: "18:00", label: "Task 9", userId: 3 },
-    ],
-    users: [
+    categories: [
       { id: 1, label: "First", color: "orange" },
       { id: 2, label: "Second", color: "purple" },
       { id: 3, label: "Third", color: "gray" },
       { id: 4, label: "Fourth", color: "aqua" },
     ],
+    timeRange: {
+      begin: new Date().valueOf(),
+      end: new Date().valueOf() + 86400000,
+    },
   },
 };
