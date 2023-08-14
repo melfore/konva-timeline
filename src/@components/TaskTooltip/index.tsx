3901e3da9377fb364085cb21f926e6ec821918fe
luciob <lucio.bordonaro@gmail.com>
2023-08-14 12:12:01 +0200

    feat: fixed standard tooltip positioning
    #25

--- src/@components/TaskTooltip/index.tsx
+++ src/@components/TaskTooltip/index.tsx
@@ -1,15 +1,20 @@
 import React, { FC } from "react";
 import { Label, Tag, Text } from "react-konva";
 
+import { useTimelineContext } from "../../@contexts/Timeline";
 import { TaskTooltipData } from "../../@utils/tasks";
 
 const TaskTooltip: FC<TaskTooltipData> = ({ task, x, y }) => {
+  const {
+    drawRange: { start: drawRangeStart },
+  } = useTimelineContext();
+
   if (!task) {
     return null;
   }
 
   return (
-    <Label x={x} y={y} opacity={0.75}>
+    <Label x={x + drawRangeStart} y={y} opacity={0.75}>
       <Tag
         fill={"black"}
         lineJoin={"round"}
