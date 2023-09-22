c7bacd0d637eec2e4e67a11d036942519dc554ed
luciob <lucio.bordonaro@gmail.com>
2023-09-22 11:26:16 +0200

    refactor: 💡 Added exports for common types and utilities
    ✅ Closes: #80

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { Resource } from "./resources/utils/resources";
+export { TaskData } from "./tasks/utils/tasks";
+export { TimeRange } from "./utils/time-range";
+export { RESOLUTIONS, Resolution } from "./utils/time-resolution";
 
 export { default as KonvaTimeline } from "./KonvaTimeline";
